89181b00c8eb496bcb83edf969bdf15a8a4c16e2
Emo2699 <eramirezm1703@alumno.ipn.mx>
2024-01-28 20:26:41 -0600

    Actualizacion 28/01/24

--- Manejo_DOM/JS/dom.js
+++ Manejo_DOM/JS/dom.js
@@ -211,3 +211,380 @@ asignados por defecto del navegador a nuestro elemento*/
 //DarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
 
 //$body.style.setProperty("background-color",DarkColor);
+
+
+/*      CONTENIDO DE LA CLASE 65        */
+//CLASES CSS
+//const $card = document.querySelector(".card");
+//console.log($card);
+
+//Acceder al valor del atributo class
+//console.log($card.className); //Devuelve el nombre de la clase que tiene el elemento
+//console.log($card.classList); //Devuelve un elemento DOMTokenList[]
+
+//Evaluar si un elemento tiene una clase en particular
+//console.log($card.classList.contains("rotate-45"));
+//
+//$card.classList.add("rotate-45"); //Añadimos una clase al elemento
+//
+//console.log($card.classList.contains("rotate-45"));
+//
+//
+//$card.classList.remove("rotate-45"); //Eliminamos una clase al elemento
+//console.log($card.classList.contains("rotate-45"));
+
+
+/*Este elemento pone la clase que le indicamos al elemento si no la 
+tiene, pero si la tiene Se la quita*/
+//$card.classList.toggle("rotate-45");//ponemos clase porque no la tiene
+//console.log($card.classList.contains("rotate-45"));
+//
+//
+//$card.classList.toggle("rotate-45");//quitamos clase porque si la tiene
+//
+//console.log($card.classList.contains("rotate-45"));
+
+//REEMPLAZO DE UNA CLASE POR OTRA
+//$card.classList.toggle("rotate-45");
+//
+//$card.classList.replace("rotate-45","rotate-135");
+
+//INTERCAMBIO O REMPLAZO DE DISTINTAS CLASES AL MISMO TIEMPO
+//$card.classList.add("opacity-80","sepia"); //Agregacion de varias clases
+//$card.classList.remove("opacity-80","sepia"); //Eliminacion de varias clases
+/*En el video se muestra que toggle funciona con más de dos clases
+sin embargo en la consola del navegador, en la seccion de elementos,
+se puede ver que solo funciono con la clase que esta en el primer
+argumento de la funcion toggle()*/
+//$card.classList.toggle("opacity-80");
+
+/*      CONTENIDO DE LA CLASE 66        */
+//Texto y HTML
+// const $whatIsDOM = document.getElementById("que-es");
+// let text = `
+//     <p>
+//         El Modelo de Objetos del Documento (<b><i>DOM - Document Object Model</i></b>) es una API para documentos HTML y XML
+//     </p>
+//     <p>
+//         Este proveé una representacion estructural del documento, permitiendo modificar su contenido y presentacion visual mediante codigo JS
+//     </p>
+//     <p>
+//         <mark>
+//             El DOM no es parte de la especificacion de JS, es una API de los navegadores.
+//         </mark>
+//     </p>
+// `;
+
+//Reemplazar contenido de la etiqueta que rescatamos por el ID
+/*Con Inner text solo agregamos texto en crudo, si ponemos etiqueta 
+HTML no la reconoce como tal*/
+//$whatIsDOM.innerText = text; //forma que se usaba para Internet Explorer, Ya no es estandar
+/*Otra forma de hacerlo es con textContent, la diferencia es que no 
+respeta los saltos de linea ni tabuladores que tenga en mi texto
+*/
+//$whatIsDOM.textContent = text;  
+
+/*Para renderizar etiquetas HTML usamos innerHTML*/
+//$whatIsDOM.innerHTML = text;
+
+/*Existe un metodo que se llama outerHTML, el cual reemplaza
+la etiqueta del elemento que lo manda a ejecutar por las 
+etiquetas que le pasemos, en este caso, lo que hay en la variable text
+
+CASO 1--> con innerHTML
+tenemos un parrafo inicial y a su contenido le puso lo que tiene la
+variable text, por lo que da un parrafo con 3 parrafos internos
+
+CASO 2 --> outerHTML
+tenemos un parrafo inicial el cual reemplazamos por completo con lo que 
+hay en la variable text, que en este caso son 3 nuevos parrafos, aunque
+pudo haber sido un H1, img, table, etc
+
+*/
+//$whatIsDOM.outerHTML = text;
+
+
+/*      CONTENIDO CLASE 67      */
+/*  RECORRIENDO EL DOM --> DOM TRAVERSING*/
+//recorrido de los elementos del DOM, es decir, de las etiquetas
+/*Trabajaremos sobre la parte de la etiqueta <section></section>*/
+//const $cards = document.querySelector(".cards");
+//console.log($cards);
+//console.log($cards.children);
+//console.log($cards.children[2]);
+//console.log($cards.parentElement); //Retorna el elemento padre
+//console.log($cards.parentNode); //Retorna el elemento hijo
+//console.log($cards.firstChild); //Primer hijo nodo
+/*Aparece como texto debido a que toma como nodo de texto al 
+salto de linea entre las etiquetas, si lo quitaramos ya 
+nos apareceria la primer tarjeta. Pasa lo mismo si tenemos un 
+comentario*/
+
+//console.log($cards.firstElementChild); //Primer elemento HTML hijo
+//Tambien hay metodos de últimos elementos e hijos
+//console.log($cards.lastChild);
+//console.log($cards.lastElementChild);
+
+//Tambien podemos ver elementos aledaños al padre
+//en este caso al estar trabajando sobre la etiqueta section
+//los elementos serian <a></a>, y <script></script>
+//console.log($cards.previousSibling); //pasa lo mismo que con los nodos
+//console.log($cards.previousElementSibling);//elemento previo de HTML
+//
+//console.log($cards.nextSibling);
+//console.log($cards.nextElementSibling);
+
+//metodo closest()
+/*Indica que elemento padre es el más cercano a partir del elemento que 
+mande a ejecutar este metodo, le tenemos que mandar como parametro 
+el nombre de la etiqueta HTML que buscamos*/
+//console.log($cards.children[2].closest("section"));
+/*Aqui mi referencia es una tarjeta.
+le estoy indicando que me devuelva la etiqueta section que actua como
+padre más cercana a la de mi referencia.*/
+
+
+
+/*      CONTENIDO DE LA CLASE 68 --> CREANDO ELEMENTOS Y FRAGMENTOS     */
+//Elemento == Etiqueta HTML
+
+//Crearemos una nueva tarjeta
+//const $figura = document.createElement("figure"), //etiqueta figure
+//$img = document.createElement("img"),//etiqueta img
+//$fidcaption = document.createElement("figcaption"),//etiqueta fidcaption
+/*La etiqueta fidcaption tiene un nodo de texto, entonces tambien lo 
+tenemos que crear*/
+//$fidcaptionText = document.createTextNode("Nueva"),
+//$cards = document.querySelector(".cards"); //Elemento padre
+
+//incorporacion de las etiquetas al DOM
+/*Primero modificamos los atributos de la etiqueta de la img,
+en donde especificamos su src y alt*/
+//$img.setAttribute("src","http://placeimg.com/200/200/animals");
+//$img.setAttribute("alt","Animals");
+
+//Aregamos al fidcaption el nodo fidcaptionText
+//$fidcaption.appendChild($fidcaptionText);
+
+//Ahora, agregamos a la etiqueta figure, las etiquetas img y fidcaption
+//$figura.appendChild($img);
+//$figura.appendChild($fidcaption);
+
+/*Para que nuestra figura tenga los estilos que tienen las demas,
+le agregamos la clase "card" ya que con esa le añadimos los estilos
+con CSS a las tarjetas*/
+//$figura.classList.add("card");
+//Ahora, agregamos a la etiqueta section la figura
+//$cards.appendChild($figura);
+
+/*Segunda forma de agregarlo, no es la más correcta pero funciona*/
+//const $figura2 = document.createElement("figure");
+
+// $figura2.innerHTML = `
+//         <img src="http://placeimg.com/200/200/people" alt="People">
+//         <figcaption>People</figcaption>
+// `;
+
+//Agregamos la clase
+//$figura2.classList.add("card");
+
+//Agregamos la figura2 a la section
+//$cards.appendChild($figura2);
+
+//Más ejemplos
+//document.write("<h3>Más ejemplos de manipulacion de DOM</h3>");
+//Estaciones del año
+//const estaciones=["Primavera","Verano","Otoño","Invierno"],
+//$ul = document.createElement("ul");
+
+//Agregamos al body nuestra lista
+//document.body.appendChild($ul);
+
+//Recorremos nuestro arreglo y por cada elemento agrgeamos un li al ul
+// estaciones.forEach(estacion =>{
+//     const $li = document.createElement("li");
+//     $li.textContent = estacion; //Agregamos el texto a la etiqueta
+//     $ul.appendChild($li); //Aregamos el "li" al "ul"
+// });
+
+
+//Segunda forma de hacerlo
+//document.write("<h3>Continentes del mundo</h3>");
+//const continentes=["America","Europa","Asia","Africa","Oceania"],
+//$ul2 = document.createElement("ul");
+
+//Agregammos al body
+//document.body.appendChild($ul2);
+/*Como iremos agregando las li como si fueran cadenas de texto
+es necesario que inicialicemos nuestro innerHTML, en caso
+contrario marcaria error*/
+//$ul2.innerHTML="";
+// continentes.forEach(continente =>{
+//     /*Le ponemos += ya que esta funcionando como cadena, si le quitamos
+//     el + solo agregaria el último elemento ya que lo estaría sobre
+//     escribiendo*/
+//     $ul2.innerHTML += `<li>${continente}</li>`
+// });
+
+/*Uso de Fragmentos*/
+// const meses = [
+//     "Enero",
+//     "Febrero",
+//     "Marzo",
+//     "Abril",
+//     "Mayo",
+//     "Junio",
+//     "Julio",
+//     "Agosto",
+//     "Septiembre",
+//     "Octubre",
+//     "Noviembre",
+//     "Diciembre"
+// ], $ul3 = document.createElement("ul"),
+// $fragmento = document.createDocumentFragment();
+
+/*Al ser fragmentos, no se puede usar innerHTML, por lo que
+se crearan elemento por elementos como en la primer
+version que se vio*/
+
+/*En el siguiente ejemplo, lo que se hace es crear un 
+fragmento donde se añaden todas las etiquetas li
+a dicho fragmento, pero aún no lo añadimos al dom.
+Es como si se creara una variable HTML que despues
+se inserta al DOM*/ 
+
+// meses.forEach(mes =>{
+
+//     const $li = document.createElement("li");
+//     $li.textContent = mes;
+//     $fragmento.appendChild($li);
+// });
+
+/*Una vez creado nuestro fragmento, lo añadimos a nuestro DOM,
+entonces, esto mejora el rendimiento ya que SOLO hacemos una
+inserción en lugar de las n que se hubieran necesitado
+de insertar elemento por elemento*/
+// document.write("<h3>Meses del año con Fragmento</h3>");
+// $ul3.appendChild($fragmento);
+// document.body.appendChild($ul3);
+
+
+/*      CONTENIDO DE LA CLASE 69 --> TEMPLATES HTML     */
+// const $card = document.querySelector(".cards")
+// ,$template = document.getElementById("plantilla").content,
+// $fragmento = document.createDocumentFragment(),
+// cardContent = [
+//     {
+//         title:"Tecnología",
+//         img:"http://placeimg.com/200/200/tech"
+//     },
+//     {
+//         title:"Animales",
+//         img:"http://placeimg.com/200/200/animals"
+//     },
+//     {
+//         title:"Arquitectura",
+//         img:"http://placeimg.com/200/200/arch"
+//     },
+//     {
+//         title:"Gente",
+//         img:"http://placeimg.com/200/200/people"
+//     },
+//     {
+//         title:"Naturalesza",
+//         img:"http://placeimg.com/200/200/nature"
+//     },
+// ];
+
+// cardContent.forEach(carta =>{
+//     $template.querySelector("img").setAttribute("src",carta.img);
+//     $template.querySelector("img").setAttribute("alt",carta.title);
+//     $template.querySelector("figcaption").textContent=carta.title;
+
+//     let $clon = document.importNode($template,true);
+//     $fragmento.appendChild($clon);
+// });
+
+// $card.appendChild($fragmento);
+
+
+
+/*   CONTENIDO DE LA CLASE 70 --> MODIFICACION DE ELEMENTOS OLD STYLE*/
+
+// const $cartas = document.querySelector(".cards"),
+// $newCarta = document.createElement("figure"),
+// $cloneCards = $cartas.cloneNode();
+
+
+// $newCarta.innerHTML=`
+//     <img src="http://placeimg.com/200/200/any" alt="Any">
+//     <figcaption>Any</figcaption>
+// `;
+//Agregamos la clase card a la etiqueta cartas
+//$newCarta.classList.add("card");
+
+/*Insertamos un elemento por remplazo*/
+//$cartas.replaceChild($newCarta,$cartas.children[2]);
+
+/*Insertamos antes de un nodo especificado*/
+//$cartas.insertBefore($newCarta,$cartas.firstElementChild);
+//$cartas.insertBefore($newCarta,$cartas.children[0]);
+
+/*Eliminamos un elemmento*/
+//$cartas.removeChild($cartas.lastElementChild);
+
+/*Clonacion de elementos*/
+//document.body.appendChild($cloneCards);
+
+
+/*CONTENIDO DE LA CLASE 71*/
+/*
+    .insertAdjacent...
+    .insertAdjacentElement(position,el)
+    .insertAdjacentHTML(position,html)
+    .insertAdjacenText(position,text)
+    
+    Posiciones
+    beforebegin(hermano anterior)
+    afterbegin(primer hijo)
+    beforeend(ultimo hijo)
+    afterend(hermano siguiente)
+*/
+
+// const $cartas = document.querySelector(".cards"),
+// $newCarta = document.createElement("figure");
+// $cloneCards = $cartas.cloneNode();
+
+
+// $newCarta.innerHTML=`
+//     <img src="http://placeimg.com/200/200/any" alt="Any">
+//     <figcaption>Any</figcaption>
+// `;
+//Agregamos la clase card a la etiqueta cartas
+//$newCarta.classList.add("card");
+
+//$cartas.insertAdjacentElement("beforebegin",$newCarta);
+//$cartas.insertAdjacentElement("afterbegin",$newCarta);
+//$cartas.insertAdjacentElement("beforeend",$newCarta);
+//$cartas.insertAdjacentElement("afterend",$newCarta);
+
+// let $contenidoCarta =`
+//     <img src="http://placeimg.com/200/200/any" alt="Any">
+//     <figcaption></figcaption>
+// `;
+
+//$newCarta.insertAdjacentHTML("beforeend",$contenidoCarta)
+
+//$newCarta.querySelector("figcaption").insertAdjacentText("beforeend","Any");
+
+//$cartas.insertAdjacentElement("beforebegin",$newCarta);
+
+
+//Con JQuery
+//$cartas.prepend($newCarta);
+//$cartas.before($newCarta);
+//$cartas.append($newCarta);
+//$cartas.after($newCarta);
+
+/*CONTENIDO DE LA CLASE 72 --> MANEJADORES DE EVENTOS*/
+
